93e71a704fc709d95d4dc4bbb52d237793280ce3
arq-e <aragwaith@gmail.com>
2022-12-06 08:28:05 +0300

    day 6

---
+++ src/main/year2022/day6.java
@@ -0,0 +1,77 @@
+package main.year2022;
+
+import main.AdventInputReader;
+
+import java.io.IOException;
+import java.util.*;
+
+public class day6 {
+
+    public static void main(String[] args) throws IOException {
+
+        List<String> input = AdventInputReader.getInput(2022, 6);
+        part1(input);
+        part2(input);
+    }
+
+    public static void part1(List<String> list) {
+        int sum = 0;
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s);
+        }
+        String str = sb.toString();
+        Deque<Character> deque = new ArrayDeque<>(4);
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < str.length();i++) {
+            deque.addLast(str.charAt(i));
+            if (set.size() > 4) {
+                deque.removeFirst();
+            }
+            if (set.size() == 4) {
+
+                set.addAll(set);
+                if (set.size() == 4) {
+                    sum = i+1;
+                    break;
+
+                }
+                set.clear();
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    public static void part2(List<String> list) {
+        int sum = 0;
+
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s);
+        }
+        String str = sb.toString();
+        Deque<Character> deque = new ArrayDeque<>(14);
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < str.length();i++) {
+            deque.addLast(str.charAt(i));
+            if (set.size() > 14) {
+                deque.removeFirst();
+            }
+            if (set.size() == 14) {
+
+                set.addAll(set);
+                if (set.size() == 14) {
+                    sum = i+1;
+                    break;
+                }
+                set.clear();
+            }
+        }
+
+
+        System.out.println(sum);
+    }
+}
